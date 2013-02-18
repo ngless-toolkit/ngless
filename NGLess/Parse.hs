dec0bd073a43f2ebcd14774ecb9724ad8e4c8f30
Luis Pedro Coelho <luis@luispedro.org>
2013-02-18 09:37:14 +0100

    ENH Skeleton for parsing
    The parsing will be Parsec based. Right now, nothing is really parsed

---
+++ NGLess/Parse.hs
@@ -0,0 +1,30 @@
+{- Copyright 2013 NGLess Authors
+ - License: GPL, version 3 or later
+ -}
+
+module Parse
+    ( parsengless
+    ) where
+
+import Language
+
+import Data.Maybe
+import Control.Applicative
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import Text.Parsec.ByteString
+import Text.Parsec.Combinator
+import Text.Parsec
+
+-- main function of this module
+-- Because the scripts are expected to be small, we can expect to load them
+-- whole into memory (with a strict ByteString) before parsing
+parsengless :: S.ByteString -> S.ByteString -> Either S.ByteString Expression
+parsengless _inputname input = case  parse nglparser "input" input of
+            Right val -> Right val
+            Left err -> Left (S8.pack . show $ err)
+
+nglparser :: Parser Expression
+nglparser = many1 expression >>= return . Sequence
+expression = symbol
+symbol = string "abcdef" >>= return . ConstSymbol . S8.pack
