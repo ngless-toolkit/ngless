1020872721e9563ed4e74c96083c17ffc31547e6
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2013-03-15 01:03:48 +0000

    Added some tests to count_bps

--- NGLess/NumberOfChars.hs
+++ NGLess/NumberOfChars.hs
@@ -1,17 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
 module NumberOfChars
     ( 
 	countBps
     ) where
 
-addChar (bpA, bpC, bpG, bpT) 'A' = (bpA + 1, bpC, bpG, bpT)
-addChar (bpA, bpC, bpG, bpT) 'C' = (bpA, bpC + 1, bpG, bpT)
-addChar (bpA, bpC, bpG, bpT) 'G' = (bpA, bpC, bpG + 1, bpT)
-addChar (bpA, bpC, bpG, bpT) 'T' = (bpA, bpC, bpG, bpT + 1)
-addChar (bpA, bpC, bpG, bpT) _ = (bpA, bpC, bpG, bpT)
+addChar (!bpA, !bpC, !bpG, !bpT) 'A' = (bpA + 1, bpC, bpG, bpT)
+addChar (!bpA, !bpC, !bpG, !bpT) 'C' = (bpA, bpC + 1, bpG, bpT)
+addChar (!bpA, !bpC, !bpG, !bpT) 'G' = (bpA, bpC, bpG + 1, bpT)
+addChar (!bpA, !bpC, !bpG, !bpT) 'T' = (bpA, bpC, bpG, bpT + 1)
+addChar (!bpA, !bpC, !bpG, !bpT) _ = (bpA, bpC, bpG, bpT)
 
-
-countChars c (x:xs) = countChars (addChar c x) xs
-countChars c [] = c 
+countChars !c (x:xs) = countChars (addChar c x) xs
+countChars !c [] = c 
 
 countBps fileContent = countBps' (0,0,0,0) (lines fileContent)
 	where
