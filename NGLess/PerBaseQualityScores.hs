a1783ff2023edbffaf88957170633eeefd08e846
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2013-09-08 03:44:44 +0100

    Change the method of the percentile calculation
    - Changed the way the calculation was being made to improve the appearance of the code.

--- NGLess/PerBaseQualityScores.hs
+++ NGLess/PerBaseQualityScores.hs
@@ -13,9 +13,9 @@ percentile50 = 0.5 :: Double
 
 --accUntilLim :: given lim, each position of the array is added until lim. Is returned the elem of the array in that position.
 accUntilLim :: [Int] -> Int -> Int
-accUntilLim array lim = accUntilLim' array 0
-    where accUntilLim' [] acc = error ("ERROR: The accumulator has value" ++ show acc ++ " and is never higher than " ++ show lim )
-          accUntilLim' (x:xs) acc = (if (acc + x) >= lim then x else accUntilLim' xs (acc + x))
+accUntilLim array lim = accUntilLim' array 0 0
+    where accUntilLim' [] acc _ = error ("ERROR: The accumulator has value" ++ show acc ++ " and is never higher than " ++ show lim )
+          accUntilLim' (x:xs) acc offset = (if (acc + x) >= lim then offset else accUntilLim' xs (acc + x) (1 + offset))
 
 
 calculateStatistics qCounts minChar = calculateStatistics' qCounts []
@@ -24,24 +24,24 @@ calculateStatistics qCounts minChar = calculateStatistics' qCounts []
           calculateStatistics' qcounts bpStatsResult = calculateStatistics' (init qcounts)
                                                                             (statistics (last qcounts) encScheme' : bpStatsResult)
 
-calcMean keySet elemSet = fromIntegral bpSum' / fromIntegral elemTotal'
+calcMean keySet elemSet elemTotal = fromIntegral bpSum' / fromIntegral elemTotal
     where eachBpValue = zipWith (*) keySet elemSet
           bpSum' = foldl1 (+) eachBpValue
-          elemTotal' = foldl1 (+) elemSet
+          
 
 --calcPerc :: Given a specific percentil,  calculates it's results. Only being used for now to calculate the Median (percentile 50%)
-calcPerc keySet perc = accUntilLim keySet val'
-    where val' = (ceiling (fromIntegral keyTotal * perc) :: Int)
-          keyTotal = (foldl1 (+) keySet)
+calcPerc keySet elemSet elemTotal perc = keySet !! (accUntilLim elemSet val')
+    where val' = (ceiling (fromIntegral elemTotal * perc) :: Int)
+          
 
 --statistics :: Calculates the Quality Statistics of a given FastQ.
 statistics :: Fractional a => Map Char Int -> Int -> (a, Int)
-statistics bpQualCount encScheme = (calcMean keySet'' elemSet , (subtract encScheme) $ calcPerc keySet' percentile50)
+statistics bpQualCount encScheme = (calcMean keySet'' elemSet' elemTotal' , (subtract encScheme) $ calcPerc keySet' elemSet' elemTotal' percentile50)
     where keySet = keys bpQualCount
-          elemSet = elems bpQualCount
           keySet' = Prelude.map (ord) keySet
           keySet'' = Prelude.map (subtract encScheme) keySet'
-
+          elemSet' = elems bpQualCount
+          elemTotal' = foldl1 (+) elemSet'
 
 
 
