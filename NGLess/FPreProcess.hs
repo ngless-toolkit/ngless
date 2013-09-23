bb7c4ce995191873f62f8de89b1a7160a220e5e6
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2013-09-23 01:06:57 +0100

    Implemented Substrim
    Started development of evaluation of Blocks

--- NGLess/FPreProcess.hs
+++ NGLess/FPreProcess.hs
@@ -1,13 +1,23 @@
 module FPreProcess
     (
-
+        FPreProcess.Read(..), substrim
     ) where
 
---TODO: receive a read of type (id:seq:qual:_:xs) and use the quality to calculateSubStrim'
---substrim :: String -> Int -> (Int,Int)
+import Data.Char
 
+data Read = Read {id :: String, seq :: String, qual::String} deriving (Show,Eq)
+
+removeBps :: String -> (Int,Int) -> String
+removeBps bps (index,size) = take size (drop index bps)
+
+--TODO: Discuss how to calculate the quality. This is not totally implemented!
+substrim :: Int -> FPreProcess.Read -> FPreProcess.Read
+substrim cutoff eachRead = do
+    let res = calculateSubStrim (map ord (qual eachRead)) cutoff
+    FPreProcess.Read (FPreProcess.id eachRead) (removeBps (FPreProcess.seq eachRead) res) (removeBps (qual eachRead) res)
 
 -- Receives a Quality array and returns a pair with the index and size of the subsequence which has the most consecutive bps respecting the cutoff.
+calculateSubStrim :: [Int] -> Int -> (Int,Int)
 calculateSubStrim qual cutoff = calculateSubStrim' qual 0 0 0 (0,0)
     where   calculateSubStrim' [] _ _ _ (index,size) = (index,size)
             calculateSubStrim' (q:xs) inc offset act_index (index, size) =
@@ -16,3 +26,5 @@ calculateSubStrim qual cutoff = calculateSubStrim' qual 0 0 0 (0,0)
                                                                          then (act_index , (1 + size))
                                                                          else (index, size))
                     else calculateSubStrim' xs 0 (offset + 1) (offset + 1) (index,size))
+
+
--- NGLess/Interpret.hs
+++ NGLess/Interpret.hs
@@ -5,11 +5,15 @@ module Interpret
     ( interpret
     ) where
 
+import qualified Data.ByteString.Lazy.Char8 as B
+
+import Control.Exception.Base
 import Language
 import Data.Text as T
 import Data.Char
 import Data.Maybe
 import ProcessFastQ
+import FPreProcess
 
 
 interpret :: [(Int,Expression)] -> IO ()
@@ -19,10 +23,8 @@ interpret ((_,e):es) = interpret' e >> interpret es
 interpret' :: Expression -> IO ()
 interpret' (Sequence es) = handleSequence es
 interpret' (Assignment var func) = variableAssignment var >> interpretFunctions func
-interpret' func@(FunctionCall _ _ _ _ ) = interpretFunctions func 
+interpret' func@(FunctionCall _ _ _ _ ) = interpretFunctions func
 interpret' e = error (Prelude.concat ["interpret: cannot handle ", show e])
---interpret' (FunctionCall Fpreprocess var _ (Just block)) = 
-
 
 variableAssignment (Variable varName) = print varName
 
@@ -30,28 +32,33 @@ variableAssignment (Variable varName) = print varName
 -- Structure -> FunctionCall FuncName Expression [(Variable, Expression)] (Maybe Block)
 
 interpretFunctions (FunctionCall Ffastq (ConstStr fname) _exprs _block) = readFastQ (T.unpack fname)
-interpretFunctions (FunctionCall Fpreprocess expr _exprs (Just _block)) = print _block
---do
---    fileReads <- (B.readFile "../Ngless-Mine/test.fq" )
---    writeFile "../Ngless-Mine/test_.fq" (fpreprocess fileReads block)
+interpretFunctions (FunctionCall Fpreprocess expr _exprs (Just _block)) = do
+    fileReads <- (B.readFile "../Ngless-Mine/test.fq" )  -- Simulate the read of a expr...
+    let fastq = createReadSet fileReads
+    writeFile "/tmp/test_.fq" (show (fpreprocess fastq _block))
+
+--TODO: interpretFunctions (FunctionCall Fsubstrim _ expr _)  = substrim (getvalue expr)
 
 interpretFunctions _ = error "interpretFunctions does not handle non-FunctionCall expressions"
 
--- >
+fpreprocess :: [FPreProcess.Read] -> Block -> [FPreProcess.Read]
+fpreprocess readSet _block = mapMaybe (interpretPreprocessBlock _block) readSet
+
+interpretPreprocessBlock :: Block -> FPreProcess.Read -> Maybe FPreProcess.Read
+interpretPreprocessBlock (Block var expr) x = Just x -- Should be interpret expr and var
 
 -- functions to handle interpretation
+createReadSet :: B.ByteString -> [FPreProcess.Read]
+createReadSet fileReads = createReadSet' (Prelude.map (B.unpack) (B.lines fileReads)) []
+    where createReadSet' (readId:readSeq:_:readQual:xs) res =  createReadSet' xs ((FPreProcess.Read readId readSeq readQual) : res)
+          createReadSet' [] res = res
+          createReadSet' _ _ = error "Number of lines is not multiple of 4!"
 
---fpreprocess fileReads block = do
---	let fileString = Prelude.map (B.unpack) (B.lines fileReads)
---	mapMaybe (interpretPreprocessBlock block) fileString-- file should come from the 'var'
-       
-       
---interpretPreprocessBlock :: Block -> String -> Maybe String
---interpretPreprocessBlock (Block var expr) str = interpret expr
 
+getvalue [(v,value)] = assert (value == "max_quality") value
+getvalue _ = error "unexpected"
 
 
 handleSequence :: [Expression] -> IO ()
 handleSequence (e:es) = interpret' e >> handleSequence es
 handleSequence [] = return ()
-
