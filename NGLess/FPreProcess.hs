505862268511b7005ba16a674fc2524f190d6d4f
Paulo Monteiro <paulo.monteiro@ist.utl.pt>
2014-03-01 16:21:34 +0000

    CHNG substrim.
    - Use directly the ByteString instead of converting.

--- NGLess/FPreProcess.hs
+++ NGLess/FPreProcess.hs
@@ -1,34 +1,54 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module FPreProcess
     ( substrim
-    , calculateSubStrim
+    , calcSubStrim
     , removeBps
     ) where
 
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+
+import Control.Monad.ST
+import Control.Monad
 
 import Data.Char
 import Language
 
 
-removeBps :: [a] -> (Int,Int) -> [a]
-removeBps bps (index,size) = take size (drop index bps)
+removeBps :: B.ByteString -> (Int,Int) -> B.ByteString
+removeBps bps (index,size) = B.take size (B.drop index bps)
 
---TODO: Discuss how to calculate the quality. This is not totally implemented!
 substrim :: Int -> NGLessObject -> NGLessObject
 substrim cutoff (NGOShortRead rId rSeq rQual) = do
-    let res = calculateSubStrim (fmap ord (B.unpack rQual)) cutoff
-    NGOShortRead rId (B.pack (removeBps (B.unpack rSeq) res)) (B.pack (removeBps (B.unpack rQual) res))
+    let res = calcSubStrim rQual (chr cutoff)
+    NGOShortRead rId (removeBps rSeq res) (removeBps rQual res)
 
 substrim _ _ = error "substrim: must have type Int and NGOShortRead"
 
 -- Receives a Quality array and returns a pair with the index and size of the subsequence which has the most consecutive bps respecting the cutoff.
-calculateSubStrim :: [Int] -> Int -> (Int,Int)
-calculateSubStrim quality cutoff = calculateSubStrim' quality 0 0 0 (0,0)
-    where   calculateSubStrim' [] _ _ _ (index,size) = (index,size)
-            calculateSubStrim' (q:xs) inc offset act_index (index, size) =
-                (if q >= cutoff
-                    then calculateSubStrim' xs (inc + 1) (offset + 1) act_index (if (inc + 1) > size
-                                                                         then (act_index , (1 + size))
-                                                                         else (index, size))
-                    else calculateSubStrim' xs 0 (offset + 1) (offset + 1) (index,size))
+calcSubStrim :: B.ByteString -> Char -> (Int,Int)
+calcSubStrim quality cutoff = fst $ B.foldl (\a b -> calcSubStrim' cutoff a b) ((0,0),(0,0)) quality
+    
+calcSubStrim' :: Char -> ((Int,Int), (Int,Int)) -> Char -> ((Int,Int), (Int,Int))    
+calcSubStrim' cutoff ((i,s),(n_i,n_s)) q = do
+    if q >= cutoff
+        then (updateIndexs', (n_i + 1, n_s + 1))
+        else ((i,s),(0,0))
+    where updateIndexs' = if (n_s + 1 > s)
+                            then (n_i + 1, n_s + 1)
+                            else (i,s)
+
+
+
+-- Function to convert B.ByteString into a Vector of Ints. NOT being used for now.
+bsToVector :: B.ByteString -> V.Vector Int
+bsToVector s = runST $ do
+    elems <- VM.unsafeNew (B.length s)
+    forM_ [0..B.length s - 1] $ \i -> do
+        let w = ord $ B.index s i
+        VM.write elems i w
+    res <- V.unsafeFreeze elems
+    return res
