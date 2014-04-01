b65089262f62b1bd5bc362774ea27b0b9f02b808
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2014-04-01 16:06:01 +0100

    -Add SamLine data structure.
    -Add operation to read all Alignements.
    Code based on git rep NGH, thanks to @luispedro.

--- NGLess/SamBamOperations.hs
+++ NGLess/SamBamOperations.hs
@@ -4,25 +4,78 @@
 
 module SamBamOperations
     (
- samToBam
+	 SamLine(..),
+	 samToBam,
+	 isAligned
     ) where
+
+
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+import Data.Bits (testBit)
+import Control.DeepSeq
 
 import Language
 import InvokeExternalProgs
 
+data SamLine = SamLine
+            { samQName :: S.ByteString
+            , samFlag :: !Int
+            , samRName :: S.ByteString
+            , samPos :: !Int
+            , samMapq :: !Int
+            , samCigar :: S.ByteString
+            , samRNext :: S.ByteString
+            , samPNext :: !Int
+            , samTLen :: !Int
+            , samSeq :: S.ByteString
+            , samQual :: S.ByteString
+            } deriving (Eq, Show)
+
+
+instance NFData SamLine where
+    rnf (SamLine qn f r p m c rn pn tl s qual) = qn `seq` f `seq` r `seq` p `seq` m `seq` c `seq` rn `seq` pn `seq` tl `seq` s `seq` qual `seq` ()
+
+
+isAligned :: SamLine -> Bool
+isAligned = not . (`testBit` 2) . samFlag
+
+
 samToBam :: FilePath -> FilePath -> IO NGLessObject
 samToBam = convertSamToBam'
 
-{-
 
-samStats :: FilePath -> IO (Int)
-samstats = calcStats . iterateSam
+readAlignments :: L.ByteString -> [SamLine]
+readAlignments = readAlignments' . L8.lines
 
-iterateSam :: FilePath -> [Bam.Bam1]
-iterateSam fp = iterateSam' $ Bam.openTamInFile fp
-	where iterateSam' handle = do
-			bam' <- Bam.get1 handle
-			case bam' of
-				Maybe a -> a : iterateSam' handle
-				Nothing -> []
--}
+readAlignments' :: [L.ByteString] -> [SamLine]
+readAlignments' [] = []
+readAlignments' (l:ls)
+    | L8.head l == '@' = readAlignments' ls
+    | otherwise = (readSamLine l:readAlignments' ls)
+
+
+readInt b = case L8.readInt b of
+    Just (v,_) -> v
+    _ -> error $ concat ["Expected int, got ", show b]
+
+readSamLine :: L.ByteString -> SamLine
+readSamLine line = case L8.split '\t' line of
+    (tk0:tk1:tk2:tk3:tk4:tk5:tk6:tk7:tk8:tk9:tk10:_) -> SamLine
+                (strict tk0)
+                (readInt tk1)
+                (strict tk2)
+                (readInt tk3)
+                (readInt tk4)
+                (strict tk5)
+                (strict tk6)
+                (readInt tk7)
+                (readInt tk8)
+                (strict tk9)
+                (strict tk10)
+    tokens -> error $ concat ["Expected 11 tokens, only got ", show $ length tokens,"\n\t\tLine was '", show line, "'"]
+
+strict :: L.ByteString -> S.ByteString
+strict = S.concat . L.toChunks
