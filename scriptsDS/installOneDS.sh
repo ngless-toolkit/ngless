eeb379967e74c5c5e0f38ae91d99312554b69eca
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2014-08-02 04:54:30 +0100

    Detect bwa/wget errors and stop execution

--- scriptsDS/installOneDS.sh
+++ scriptsDS/installOneDS.sh
@@ -33,14 +33,41 @@ mus_musculus=Mus_musculus.GRCm38
 bwa_v=0.7.7
 bwa=http://sourceforge.net/projects/bio-bwa/files/bwa-$bwa_v.tar.bz2/download
 
+download(){
+  echo "start downloading" $2
+  wget -O $1 $2 &> /dev/null;
+  r=$? 
+  if [[ $r != 0 ]] ; then
+    rm -rf $3
+    echo "ERROR downloading " $2 "."
+    exit 3 
+  fi
+  echo "ended downloading" $1
+  
+}
+
+generateindex(){
+  echo "start generating index"
+  ../../../bwa-$bwa_v/bwa index "genome.fa.gz";
+  r=$?
+  if [[ $r != 0 ]] ; then
+    cd ../../../ ;rm -rf $1;
+    echo "ERROR in " $1 ". Deleting content."
+    exit 3 
+  fi
+  cd ../../../;
+  echo "generated index."
+
+}
+
 genomepath(){
   base_name=$1
   mkdir -p $2/Sequence/BWAIndex && cd $_; \
   echo "Start Downloading ${!base_name}.$version.dna.toplevel.fa.gz..."
-  wget -O genome.fa.gz "ftp://ftp.ensembl.org/pub/release-75/fasta/$1/dna/${!base_name}.$version.dna.toplevel.fa.gz" &> /dev/null; \
+  download genome.fa.gz  "ftp://ftp.ensembl.org/pub/release-75/fasta/$1/dna/${!base_name}.$version.dna.toplevel.fa.gz" $2
   echo "Completed ${!base_name}.$version.dna.toplevel.fa.gz..."
   echo "Start indexing..."
-  ../../../bwa-$bwa_v/bwa index "genome.fa.gz"; cd  ../../../;
+  generateindex $2
   echo "Completed indexing"
 }
 
@@ -48,15 +75,15 @@ gtfpath(){
   base_name=$1
   mkdir -p $2/Annotation && cd $_; \
   echo "Start Downloading ${!base_name}.$version.gtf.gz..."
-  wget -O annot.gtf.gz "ftp://ftp.ensembl.org/pub/release-75/gtf/$1/${!base_name}.$version.gtf.gz" &> /dev/null; \
+  download annot.gtf.gz "ftp://ftp.ensembl.org/pub/release-75/gtf/$1/${!base_name}.$version.gtf.gz" $2
   echo "Completed ${!base_name}.$version.gtf.gz"
   cd ../../;
 }
 
 installbwa(){
 	if [ ! -d "bwa-$bwa_v" ]; then
-  		wget -O bwa-$bwa_v.tar.bz2 http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.7.tar.bz2/download &> /dev/null
-  		tar -xvf bwa-$bwa_v.tar.bz2
+  		download bwa-$bwa_v.tar.bz2 http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.7.tar.bz2/download $2
+      tar -xvf bwa-$bwa_v.tar.bz2
   		echo "[BWA] installed"
   		cd bwa-$bwa_v; make; cd ..
   	else
