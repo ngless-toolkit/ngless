cebc546d256d7f6d5922ef0d428ccdfcbeee6a84
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2014-08-08 04:45:46 +0100

    Add function to detect if file is already calculated

--- test_samples/htseq-res/generateHtseqFiles.sh
+++ test_samples/htseq-res/generateHtseqFiles.sh
@@ -5,6 +5,24 @@ fname=$dirname.tar.gz
 
 url=http://pypi.python.org/packages/source/H/HTSeq/$fname#md5=b7f4f38a9f4278b9b7f948d1efbc1f05
 
+isNotCompleted () {
+  echo "File: $1 has $2 lines."
+  if fileExists $1; then
+     ! fileSizeCorrect $1 $2
+    else
+    return $?
+  fi
+}
+
+fileSizeCorrect(){
+    v=$(wc -l < $1)
+    [[ $v -eq $2 ]]
+}
+
+fileExists(){ 
+  [ -f "$1" ]
+}
+
 #remove last 5 lines of htseq, which has statistics.
 filterEndL (){
   sed -n -e :a -e "1,$1!{P;N;D;};N;ba" $2
@@ -40,13 +58,13 @@ downloadURL $url
 
 # -a 0 allows to ignore all htseq filter on quality.
 # Test features
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m union | filterEndL 5 > htseq_gene_noStrand_union.txt 
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t exon -m union | filterEndL 5 > htseq_exon_noStrand_union.txt 
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t CDS  -m union | filterEndL 5 >  htseq_cds_noStrand_union.txt 
+isNotCompleted htseq_gene_noStrand_union.txt 7126 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m union | filterEndL 5 > htseq_gene_noStrand_union.txt
+isNotCompleted htseq_exon_noStrand_union.txt 7126 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t exon -m union | filterEndL 5 > htseq_exon_noStrand_union.txt
+isNotCompleted htseq_cds_noStrand_union.txt 6692 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t CDS  -m union | filterEndL 5 >  htseq_cds_noStrand_union.txt
 
 # Test mode
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m intersection-strict   | filterEndL 5 > htseq_gene_noStrand_inters-strict.txt 
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m intersection-nonempty | filterEndL 5 > htseq_gene_noStrand_inters-nempty.txt 
+isNotCompleted htseq_gene_noStrand_inters-strict.txt 7126 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m intersection-strict   | filterEndL 5 > htseq_gene_noStrand_inters-strict.txt 
+isNotCompleted htseq_gene_noStrand_inters-nempty.txt 7126 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s no -t gene -m intersection-nonempty | filterEndL 5 > htseq_gene_noStrand_inters-nempty.txt 
 
 # Test strand Positive. Negative tested before
-python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s yes -t gene -m union | filterEndL 5 > htseq_gene_yesStrand_union.txt 
+isNotCompleted htseq_gene_yesStrand_union.txt 7126 && python -m HTSeq.scripts.count ../sample.sam ../sample.gtf -a 0 -s yes -t gene -m union | filterEndL 5 > htseq_gene_yesStrand_union.txt 
