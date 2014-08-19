5a01cfc9b41cce50181a9cd62f158f062e164241
Luis Pedro Coelho <luis@luispedro.org>
2014-08-19 16:36:35 +0200

    TST Fix ht-seq script
    This is a bash script, declare it as such.

--- test_samples/htseq-res/generateHtseqFiles.sh
+++ test_samples/htseq-res/generateHtseqFiles.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/bash
 
 dirname=HTSeq-0.6.1
 fname=$dirname.tar.gz
@@ -7,7 +7,7 @@ url=http://pypi.python.org/packages/source/H/HTSeq/$fname#md5=b7f4f38a9f4278b9b7
 
 isNotCompleted () {
   echo "File: $1 has $2 lines."
-  if fileExists $1; then
+  if [ -f $1 ]; then
      ! fileSizeCorrect $1 $2
     else
     return 0
@@ -16,11 +16,7 @@ isNotCompleted () {
 
 fileSizeCorrect(){
     v=$(wc -l < $1)
-    [[ $v -eq $2 ]]
-}
-
-fileExists(){ 
-  [ -f "$1" ]
+    test $v -eq $2
 }
 
 #remove last 5 lines of htseq, which has statistics.
@@ -30,34 +26,34 @@ filterEndL (){
 
 download(){
   echo "start downloading" $2
-  wget -O $1 $2 &> /dev/null;
-  r=$? 
-  if [[ $r != 0 ]] ; then
+  wget -O $1 $2 &>/dev/null
+  r=$?
+  if [ $r != 0 ] ; then
     rm -rf $1
     echo "ERROR downloading " $2 "."
-    exit 3 
+    exit 3
   fi
   echo "ended downloading" $1
 }
 
 
-
 downloadURL(){
-	if [ ! -d "$dirname" ]; then
-  		download $fname $1
-      	tar -xzvf $fname
-  		cd $dirname; python setup.py install --user;
-      if [[ $r != 0 ]] ; then
-        rm -rf $1
-        echo "ERROR installing HTSEQ."
-        exit 3 
-      fi
-      cd ..
-  		echo "[HTSEQ] installed"
-  	else
-  		echo "[HTSEQ] Already installed"
-	fi
-	rm -rf $fname
+    if [ ! -d "$dirname" ]; then
+        download $fname $1
+        echo tar -xzvf $fname
+        tar -xzvf $fname
+        cd $dirname; python setup.py install --user;
+        if [ $r != 0 ]; then
+            rm -rf $1
+            echo "ERROR installing HTSEQ."
+            exit 3
+        fi
+        cd ..
+        echo "[HTSEQ] installed"
+    else
+        echo "[HTSEQ] Already installed"
+    fi
+    rm -rf $fname
 }
 
 downloadURL $url
