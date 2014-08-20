b5d97f37982f3709dbcd0ddd6c3e50239b5e591f
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2014-08-20 16:04:37 +0100

    ADD homo sapiens to the default data sets

--- scriptsDS/installDS.sh
+++ scriptsDS/installDS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ds=("saccharomyces_cerevisiae" "caenorhabditis_elegans" "drosophila_melanogaster" "gallus_gallus" "canis_familiaris" "rattus_norvegicus" "bos_taurus" "mus_musculus")
+ds=("homo_sapiens" "saccharomyces_cerevisiae" "caenorhabditis_elegans" "drosophila_melanogaster" "gallus_gallus" "canis_familiaris" "rattus_norvegicus" "bos_taurus" "mus_musculus")
 
 for i in "${ds[@]}"
 do
