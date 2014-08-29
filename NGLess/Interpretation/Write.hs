7f5ac82ef4671c36d838c18a7badb0e81f629f2c
Luis Pedro Coelho <luis@luispedro.org>
2014-08-29 15:58:20 +0200

    ENH Avoid loading data into memory
    General refactoring as well

--- NGLess/Interpretation/Write.hs
+++ NGLess/Interpretation/Write.hs
@@ -14,6 +14,7 @@ import System.Directory (canonicalizePath)
 import System.Process
 import System.Exit
 import System.IO
+import Data.Maybe
 
 import Language
 import FileManagement
@@ -34,10 +35,10 @@ writeToUncFile (NGOReadSet path enc tmplate) newfp = do
     readPossiblyCompressedFile path >>= BL.writeFile newfp'
     return $ NGOReadSet (B.pack newfp') enc tmplate
 
-writeToUncFile err _ = error ("writeToUncFile: Should have received a NGOReadSet or a NGOMappedReadSet but the type was: " ++ (show err))
+writeToUncFile obj _ = error ("writeToUncFile: Should have received a NGOReadSet or a NGOMappedReadSet but the type was: " ++ (show obj))
 
 
-writeToFile :: NGLessObject -> [(T.Text, NGLessObject)] -> IO NGLessObject   
+writeToFile :: NGLessObject -> [(T.Text, NGLessObject)] -> IO NGLessObject
 writeToFile (NGOList el) args = do
       let templateFP = getNGOString $ lookup "ofile" args
           newFPS' = map (\x -> T.replace "{index}" x templateFP) indexFPs
@@ -48,17 +49,15 @@ writeToFile (NGOList el) args = do
         fp' fp = M.toList $ M.insert "ofile" (NGOString fp) (M.fromList args)
 
 writeToFile el@(NGOReadSet _ _ _) args = writeToUncFile el $ getNGOString ( lookup "ofile" args )
-
 writeToFile el@(NGOMappedReadSet fp defGen) args = do
     let newfp = getNGOString (lookup (T.pack "ofile") args) --
-        format = lookup "format" args
+        format = fromMaybe (NGOSymbol "sam") (lookup "format" args)
     case format of
-        Nothing -> writeToUncFile el newfp
-        Just format' -> case format' of 
-            (NGOSymbol "bam") -> convertSamToBam (T.unpack fp) (T.unpack newfp) 
-                                            >>= \x -> return $ NGOMappedReadSet x defGen --newfp will contain the bam
-            _     -> writeToUncFile el newfp -- Sam file is the kept format so no need to convert.
-
+        (NGOSymbol "sam") -> writeToUncFile el newfp
+        (NGOSymbol "bam") -> do
+                        newfp' <- convertSamToBam (T.unpack fp) (T.unpack newfp)
+                        return (NGOMappedReadSet newfp' defGen) --newfp will contain the bam
+        _ -> error "This format should have been impossible"
 writeToFile (NGOAnnotatedSet fp) args = do
     let newfp = getNGOString $ lookup "ofile" args
         del = getDelimiter  $ lookup "format" args
@@ -72,12 +71,9 @@ writeToFile (NGOAnnotatedSet fp) args = do
     where
         writeAnnotResWDel' p cont = do
             BL.writeFile (T.unpack p) cont
-            canonicalizePath (T.unpack p) >>= insertCountsProcessedJson 
+            canonicalizePath (T.unpack p) >>= insertCountsProcessedJson
             return $ NGOAnnotatedSet p
-            
 writeToFile _ _ = error "Error: writeToFile Not implemented yet"
-
-
 
 getDelimiter :: Maybe NGLessObject -> B.ByteString
 getDelimiter x = case x of
@@ -86,28 +82,17 @@ getDelimiter x = case x of
         (Just err) ->  error ("Type must be NGOSymbol, but was given" ++ (show err))
         Nothing -> "\t"
 
-convertSamToBam samfp newfp = do
-    printNglessLn $ "Start to convert Sam to Bam. from " ++ samfp ++ " to -> " ++ newfp
-    jHandle <- convSamToBam' samfp newfp
-    exitCode <- waitForProcess jHandle
-    case exitCode of
-       ExitSuccess -> return (T.pack newfp)
-       ExitFailure err -> error ("Failure on converting sam to bam" ++ (show err))
-
-convSamToBam' samFP newfp = do
+convertSamToBam samfile newfp = do
+    printNglessLn $ "Start to convert Sam to Bam. from " ++ samfile ++ " to -> " ++ newfp
     samPath <- samtoolsBin
-    (_, Just hout, Just herr, jHandle) <- createProcess (
-        proc samPath
-            ["view", "-bS" ,samFP ]
-        ){ std_out = CreatePipe,
-           std_err = CreatePipe }
-    writeToFile' hout newfp
-    hGetContents herr >>= printNglessLn
-    return jHandle
-
-writeToFile' :: Handle -> FilePath -> IO ()
-writeToFile' handle path = do
-    contents <- B.hGetContents handle
-    B.writeFile path contents
-    hClose handle
-
+    withFile newfp ReadMode $ \hout -> do
+        (_, _, Just herr, jHandle) <- createProcess (
+            proc samPath
+                ["view", "-bS", samfile]
+            ){ std_out = UseHandle hout,
+               std_err = CreatePipe }
+        hGetContents herr >>= putStrLn
+        exitCode <- waitForProcess jHandle
+        case exitCode of
+           ExitSuccess -> return (T.pack newfp)
+           ExitFailure err -> error ("Failure on converting sam to bam" ++ (show err))
