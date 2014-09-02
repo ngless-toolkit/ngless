006f8dde57dd0f930c58d9f09ed9f67990095dca
Luis Pedro Coelho <luis@luispedro.org>
2014-09-02 11:55:42 +0200

    RFCT Move NGLess functionality out of Data/

--- NGLess/CountOperation.hs
+++ NGLess/CountOperation.hs
@@ -4,11 +4,13 @@ module CountOperation
       , filterAnnot
     ) where
 
+import qualified Data.Text as T
 import qualified Data.ByteString.Lazy.Char8 as L8
 
 import Language
 import FileManagement (readPossiblyCompressedFile)
 
+import Data.GFF
 import Data.AnnotRes
 
 countAnnotatedSet :: FilePath -> Maybe NGLessObject -> NGLessObject -> IO FilePath
@@ -19,5 +21,25 @@ countAnnotatedSet p f m = do
 
 filterAnnot :: L8.ByteString -> Maybe NGLessObject -> NGLessObject -> [GffCount]
 filterAnnot fc f m = filter (filterAnnot') $ readAnnotCounts fc
-    where 
+    where
       filterAnnot' c = (isMinAmount m c) && (filterCounts f c)
+
+
+--- Quite similar to the filterFeatures in GFF but access different field name.
+filterCounts :: Maybe NGLessObject -> GffCount -> Bool
+filterCounts feats annotL = maybe True (fFeat annotL) feats
+  where
+        fFeat g (NGOList f) = foldl (\a b -> a || b) False (map (filterCounts' g) f)
+        fFeat _ err = error("Type should be NGOList but received: " ++ (show err))
+
+filterCounts' :: GffCount -> NGLessObject -> Bool
+filterCounts' g (NGOSymbol "gene") = (==GffGene) . annotType $ g
+filterCounts' g (NGOSymbol "exon") = (==GffExon) . annotType $ g
+filterCounts' g (NGOSymbol "cds" ) = (==GffCDS) . annotType  $ g
+filterCounts' g (NGOSymbol "CDS" ) = (==GffCDS) . annotType  $ g
+filterCounts' g (NGOSymbol s) = (show . annotType $ g) == (T.unpack s)
+filterCounts' _ err = error ("Type should be NGOList but received: " ++ (show err))
+
+isMinAmount :: NGLessObject -> GffCount ->  Bool
+isMinAmount (NGOInteger l) g = (toInteger $ annotCount g) >= l
+isMinAmount err _ = error ("Type should be NGOInteger but received: " ++ (show err))
--- NGLess/Data/AnnotRes.hs
+++ NGLess/Data/AnnotRes.hs
@@ -4,15 +4,12 @@ module Data.AnnotRes
     ( 
       GffCount(..)
       , readAnnotCounts
-      , filterCounts
-      , isMinAmount
       , writeAnnotCount
       , showGffCountDel
       , showUniqIdCounts
       , filterByStrand
     ) where
 
-import qualified Data.Text as T
 import qualified Data.ByteString as B
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
@@ -25,7 +22,6 @@ import Control.DeepSeq
 import System.FilePath.Posix((</>), splitFileName)
 
 import FileManagement
-import Language
 
 import Configuration
 import Data.GFF
@@ -75,27 +71,6 @@ readAnnotLine line = if length tokens == 4
     where
         tokens = L8.split '\t' line
         [tk0,tk1,tk2,tk3] = tokens
-
-
---- Quite similar to the filterFeatures in GFF but access different field name.
-filterCounts :: Maybe NGLessObject -> GffCount -> Bool
-filterCounts feats annotL = maybe True (fFeat annotL) feats
-  where 
-        fFeat g (NGOList f) = foldl (\a b -> a || b) False (map (filterCounts' g) f)
-        fFeat _ err = error("Type should be NGOList but received: " ++ (show err))
-
-filterCounts' :: GffCount -> NGLessObject -> Bool
-filterCounts' g (NGOSymbol "gene") = (==GffGene) . annotType $ g
-filterCounts' g (NGOSymbol "exon") = (==GffExon) . annotType $ g
-filterCounts' g (NGOSymbol "cds" ) = (==GffCDS) . annotType  $ g
-filterCounts' g (NGOSymbol "CDS" ) = (==GffCDS) . annotType  $ g
-filterCounts' g (NGOSymbol s) = (show . annotType $ g) == (T.unpack s)
-filterCounts' _ err = error ("Type should be NGOList but received: " ++ (show err))
-
-
-isMinAmount :: NGLessObject -> GffCount ->  Bool
-isMinAmount (NGOInteger l) g = (toInteger $ annotCount g) >= l
-isMinAmount err _ = error ("Type should be NGOInteger but received: " ++ (show err))
 
 writeAnnotCount :: FilePath -> [GffCount]-> IO FilePath
 writeAnnotCount fn im = do
