ce2ae283fac787d7564aa675ea57a4a6c9a3f04d
Luis Pedro Coelho <luis@luispedro.org>
2015-01-07 16:39:10 +0100

    DOC Update examples to newer ngless

--- examples/oneLiner.hs
+++
@@ -1,2 +0,0 @@
-ngless "0.0"
-write(count(annotate(map(fastq('../samples/sample_1.fq'),reference='sacCer3'), features=[{gene},{cds},{exon}], gff='../samples/genes.gff'), count={gene}, min=10), ofile="../samples/CountsResult.txt", format={tsv})
