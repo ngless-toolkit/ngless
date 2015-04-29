02859ae98f32c069dec185f5d6430b95f3dcb97c
Luis Pedro Coelho <luis@luispedro.org>
2015-04-29 17:54:49 +0200

    ENH Write paired and paired+single fastq files
    This implied that new semantics were added to the write() function

--- NGLess/Interpretation/Write.hs
+++ NGLess/Interpretation/Write.hs
@@ -6,6 +6,7 @@
 
 module Interpretation.Write
     ( writeToFile
+    , _formatFQOname
     ) where
 
 
@@ -19,6 +20,7 @@ import System.Process
 import System.Exit
 import System.IO
 import Data.String.Utils
+import Data.List (isInfixOf)
 import Data.Maybe
 
 import Language
@@ -27,6 +29,19 @@ import JSONManager
 import Configuration
 import Output
 import Data.AnnotRes
+
+hasSubstring :: String -> String -> Bool
+hasSubstring = flip isInfixOf
+
+removeEnd :: String -> String -> String
+removeEnd base suffix = take (length base - length suffix) base
+
+_formatFQOname :: FilePath -> FilePath -> FilePath
+_formatFQOname base insert
+    | hasSubstring "{index}" base = replace base "{index}" ("." ++ insert ++ ".")
+    | endswith ".fq" base = (removeEnd base ".fq") ++ "." ++ insert ++ ".fq"
+    | endswith ".fq.gz" base = (removeEnd base ".fq") ++ "." ++ insert ++ ".fq.gz"
+    | otherwise = error ("Cannot handle " ++ base)
 
 getNGOPath (Just (NGOFilename p)) = p
 getNGOPath (Just (NGOString p)) = T.unpack p
@@ -55,6 +70,19 @@ writeToFile (NGOList el) args = do
         fp' fp = M.toList $ M.insert "ofile" (NGOString fp) (M.fromList args)
 
 writeToFile el@NGOReadSet1{} args = writeToUncFile el $ getNGOPath (lookup "ofile" args)
+writeToFile (NGOReadSet2 enc r1 r2) args = do
+    let newfp = getNGOPath (lookup "ofile" args)
+    NGOReadSet1 _ r1' <- writeToUncFile (NGOReadSet1 enc r1) (_formatFQOname newfp "pair.1")
+    NGOReadSet1 _ r2' <- writeToUncFile (NGOReadSet1 enc r2) (_formatFQOname newfp "pair.2")
+    return (NGOReadSet2 enc r1' r2')
+
+writeToFile (NGOReadSet3 enc r1 r2 r3) args = do
+    let newfp = getNGOPath (lookup "ofile" args)
+    NGOReadSet1 _ r1' <- writeToUncFile (NGOReadSet1 enc r1) (_formatFQOname newfp "pair.1")
+    NGOReadSet1 _ r2' <- writeToUncFile (NGOReadSet1 enc r2) (_formatFQOname newfp "pair.2")
+    NGOReadSet1 _ r3' <- writeToUncFile (NGOReadSet1 enc r3) (_formatFQOname newfp "singles")
+    return (NGOReadSet3 enc r1' r2' r3')
+
 writeToFile el@(NGOMappedReadSet fp defGen) args = do
     let newfp = getNGOPath (lookup "ofile" args) --
         format = fromMaybe (NGOSymbol "sam") (lookup "format" args)
