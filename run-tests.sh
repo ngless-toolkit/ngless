450ffeb7d881884e11761152eab0ed692c0535d1
Luis Pedro Coelho <luis@luispedro.org>
2015-04-29 17:59:30 +0200

    TST Check for error in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,9 @@
 make check
 
 basedir=$PWD
+ok="yes"
 for testdir in tests/*; do
-    if test -d $testdir; then 
+    if test -d $testdir; then
         echo "Running $testdir"
         cd $testdir
         mkdir -p temp
@@ -12,10 +13,19 @@ for testdir in tests/*; do
         rm -rf temp
         for f in expected.*; do
             diff -u $f output${f#expected}
+            if test $? -ne "0"; then
+               echo "ERROR IN TEST"
+               ok=no
+            fi
         done
         rm -rf *.output_ngless
         cd $basedir
     fi
 done
 
-echo "All done."
+if test $ok = "yes"; then
+    echo "All done."
+else
+    echo "An error occurred."
+    exit 1
+fi
