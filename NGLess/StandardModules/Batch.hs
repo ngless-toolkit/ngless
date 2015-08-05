08988d7f03a71f0914db8ebea516b77a88020c2b
Luis Pedro Coelho <luis@luispedro.org>
2015-08-05 18:18:03 +0200

    ENH Add batch module

---
+++ NGLess/StandardModules/Batch.hs
@@ -0,0 +1,57 @@
+{- Copyright 2015 NGLess Authors
+ - License: MIT
+ -}
+
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+
+module StandardModules.Batch
+    ( loadModule
+    ) where
+
+import System.Environment (lookupEnv)
+import Control.Concurrent (setNumCapabilities)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Text as T
+import Control.Applicative
+import Control.Monad
+import Text.Read
+import Data.Maybe
+
+import Language
+import Modules
+import NGLess
+
+getLSFncpus :: IO (Maybe Int)
+getLSFncpus = (>>= readMaybe) <$> lookupEnv "LSB_DJOB_NUMPROC"
+
+getLSFjobIndex :: IO (Maybe Integer)
+getLSFjobIndex = (>>= readMaybe) <$> lookupEnv "LSB_JOBINDEX"
+
+getGEncpus :: IO (Maybe Int)
+getGEncpus = (>>= readMaybe) <$> lookupEnv "NSLOTS"
+
+getGEjobIndex :: IO (Maybe Integer)
+getGEjobIndex = (>>= readMaybe) <$> lookupEnv "SGE_TASK_ID"
+
+
+getJobIndex :: IO (Maybe Integer)
+getJobIndex = liftA2 (<|>) getLSFjobIndex getGEjobIndex
+
+getNcpus :: IO (Maybe Int)
+getNcpus = liftA2 (<|>) getLSFncpus getGEncpus
+
+loadModule :: T.Text -> NGLessIO Module
+loadModule _ = liftIO $ do
+    job_id <- getJobIndex
+    ncpus <- getNcpus
+    when (isJust ncpus) $
+        setNumCapabilities (fromJust ncpus)
+    return Module
+        { modInfo = ModInfo "stdlib.batch" "0.0"
+        , modConstants =
+                [("JOBINDEX_OR_0", NGOInteger (fromMaybe 0 job_id))
+                ,("JOBINDEX_VALID", NGOBool (isJust job_id))
+                ]
+        , modFunctions = []
+        , runFunction = \_ _ _ -> return NGOVoid
+        }
--- NGLess/StandardModules/NGLStdlib.hs
+++ NGLess/StandardModules/NGLStdlib.hs
@@ -9,6 +9,7 @@ module StandardModules.NGLStdlib
 
 
 import qualified StandardModules.Example as Example
+import qualified StandardModules.Batch as Batch
 import Modules
 import NGLess
 
@@ -16,5 +17,6 @@ loadStdlibModules :: [ModInfo] -> NGLessIO [Module]
 loadStdlibModules = mapM loadModules1
 
 loadModules1 (ModInfo "example" version) = Example.loadModule version
+loadModules1 (ModInfo "batch" version) = Batch.loadModule version
 loadModules1 (ModInfo modname _) = error ("Could not load module " ++show modname)
 
