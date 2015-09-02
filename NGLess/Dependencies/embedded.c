f9758d54c9f84798d61fb9ed5ab53c0c55fd2834
Luis Pedro Coelho <luis@luispedro.org>
2015-09-02 15:00:25 +0200

    BUG Match newer executable name in embedded file

--- NGLess/Dependencies/embedded.c
+++ NGLess/Dependencies/embedded.c
@@ -3,5 +3,5 @@
 const unsigned char* get_samtools_data () { return samtools_1_0_samtools; }
 unsigned int get_samtools_len () { return samtools_1_0_samtools_len; }
 
-const unsigned char* get_bwa_data () { return bwa_0_7_7_bwa; }
-unsigned int get_bwa_len () { return bwa_0_7_7_bwa_len; }
+const unsigned char* get_bwa_data () { return bwa_0_7_7_ngless_bwa_static; }
+unsigned int get_bwa_len () { return bwa_0_7_7_ngless_bwa_static_len; }
