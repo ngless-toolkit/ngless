f8ccafc1c35365d483892fe0de6710e6dac55dac
Luis Pedro Coelho <luis@luispedro.org>
2015-09-09 15:05:46 +0200

    ENH script to transform MOCAT files into standard GTF

---
+++ Modules/motus.ngm/create-gtf-from-mocat-file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# converts data in MOCAT .coods & map files to the standard GTF format
+
+# Load coordinates
+coords = {}
+for line in open('motus_data/data/mOTU.v1.padded.coord'):
+    gene,start,end = line.strip().split()
+    coords[gene] = (start,end)
+
+# Each line in the map file becomes on GTF line
+for line in open('motus_data/data/mOTU.v1.padded.motu.map'):
+    gene, _, cog, cog_cluster_id = line.strip().split('\t')
+    start,end = coords[gene]
+    print("\t".join([
+            gene,
+            "mOTU",
+            "gene",
+            start,
+            end,
+            ".",
+            ".",
+            ".",
+            "gene_id \"{}.{}\"".format(cog, cog_cluster_id)]))
+
