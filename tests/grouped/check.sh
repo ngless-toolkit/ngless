791266bf5e8b98d662c0bd65f8fd094308178a26
Luis Pedro Coelho <luis@luispedro.org>
2016-01-08 14:17:52 +0100

    TST Fix direction of check

--- tests/grouped/check.sh
+++ tests/grouped/check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if diff <(grep -v '^@PG' one.sam) <(grep -v '^@PG' split.sam ) >/dev/null; then
+if ! diff <(grep -v '^@PG' one.sam) <(grep -v '^@PG' split.sam ) ; then
     exit 1
 fi
-rm one.sam split.sam
+rm -f one.sam split.sam
