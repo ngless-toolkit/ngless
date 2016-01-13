7d8b7cd0ed09790690912f1a40951c3b8aec0c96
Luis Pedro Coelho <luis@luispedro.org>
2016-01-13 13:56:43 +0100

    MIN Remove deprecated object

--- NGLess/Utils/Network.hs
+++ NGLess/Utils/Network.hs
@@ -10,6 +10,7 @@ import Data.Conduit.Binary (sinkFile)
 import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 import qualified Data.ByteString.Char8 as B
 import qualified Network.HTTP.Conduit as HTTP
+import qualified Network.HTTP.Client as HTTP
 
 import Utils.ProgressBar
 
@@ -17,12 +18,12 @@ downloadFile :: String -> FilePath -> IO ()
 downloadFile url destPath = do
     putStrLn ("Downloading "++url)
     req <- HTTP.parseUrl url
-    manager <- HTTP.newManager HTTP.conduitManagerSettings
+    manager <- HTTP.newManager HTTP.defaultManagerSettings
     runResourceT $ do
         res <- HTTP.http req manager
         case lookup "Content-Length" (HTTP.responseHeaders res) of
             Nothing -> error "HTTP Response does not contain Content-Length header"
-            Just csize -> do
+            Just csize ->
                 HTTP.responseBody res $$+-
                     printProgress (read (B.unpack csize)) =$
                     sinkFile destPath
@@ -32,12 +33,13 @@ printProgress csize = do
     pbar <- liftIO (mkProgressBar 40)
     loop 0 pbar
   where
-    loop len pbar = await >>= maybe (return ()) (\bs -> do
+    loop !len pbar = await >>= \case
+        Nothing -> return ()
+        Just bs -> do
             let len' = len + B.length bs
-                progress = (fromIntegral len' / fromIntegral csize)
+                progress = fromIntegral len' / fromIntegral csize
             pbar' <- liftIO (updateProgressBar pbar progress)
             yield bs
             loop len' pbar'
-            )
 
 
