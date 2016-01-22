a7c16039415f885a1242ffd6ac8aa0c5a0e623e5
Luis Pedro Coelho <luis@luispedro.org>
2016-01-22 22:25:13 +0100

    MIN Add type signature to 'toFractions'

--- NGLess/Utils/Vector.hs
+++ NGLess/Utils/Vector.hs
@@ -20,15 +20,18 @@ unsafeIncrement v i = unsafeIncrement' v i 1
 unsafeIncrement' :: (Num a, PrimMonad m, VM.Unbox a) => VM.MVector (PrimState m) a -> Int -> a -> m ()
 unsafeIncrement' v i inc = unsafeModify v (+ inc) i
 
+-- in vector >= 0.11, this function is already there:
 unsafeModify :: (PrimMonad m, VM.Unbox a) => VM.MVector (PrimState m) a -> (a -> a) -> Int -> m ()
 unsafeModify v f i = do
     c <- VM.unsafeRead v i
     VM.unsafeWrite v i (f c)
 
 
+toFractions :: (PrimMonad m, VM.Unbox a, Fractional a, Eq a, Num a) => VM.MVector (PrimState m) a -> m ()
 toFractions v = do
     v' <- V.unsafeFreeze v
     let total = V.foldr1 (+) v'
         n = VM.length v
-    forM_ [0..n - 1] $ \i ->
-        unsafeModify v (/ total) i
+    when (total /= 0) $
+        forM_ [0..n - 1] $ \i ->
+            unsafeModify v (/ total) i
