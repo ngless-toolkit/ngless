58657b85c3427585c76209433c13b628b012f110
Luis Pedro Coelho <luis@luispedro.org>
2016-02-11 16:08:35 +0100

    MIN Add groupC combinator
    This conduit groups its input into groups of n elements

--- NGLess/Utils/Conduit.hs
+++ NGLess/Utils/Conduit.hs
@@ -7,6 +7,7 @@ module Utils.Conduit
     , conduitPossiblyCompressedFile
     , asyncMapC
     , linesC
+    , groupC
     ) where
 
 import qualified Data.ByteString as B
@@ -17,6 +18,7 @@ import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Sequence as Seq
 import           Data.Sequence ((|>), ViewL(..))
+import           Control.Monad (unless)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Exception (evaluate)
 import           Control.DeepSeq
@@ -62,3 +64,14 @@ asyncMapC maxSize f = initLoop (0 :: Int) (Seq.empty :: Seq.Seq (A.Async b))
                             C.yield =<< liftIO (A.wait r)
                             loop (rest |> v')
                         _ -> error "should never happen"
+
+-- | groupC yields the input as groups of 'n' elements. If the input is not a
+-- multiple of 'n', the last element will be incomplete
+groupC :: (Monad m) => Int -> C.Conduit a m [a]
+groupC n = loop n []
+    where
+        loop 0 ps = C.yield (reverse ps) >> loop n []
+        loop c ps = C.await >>= \case
+            Nothing -> unless (null ps) $ C.yield (reverse ps)
+            Just p -> loop (c-1) (p:ps)
+
