8c9b6a97f20b2d58bac0304a40e1986181d7862c
Luis Pedro Coelho <luis@luispedro.org>
2016-02-18 00:05:26 +0100

    DOC Little nix expression to build docs

---
+++ docs/contents/default.nix
@@ -0,0 +1,41 @@
+#!/usr/bin/env nix-shell
+
+with import <nixpkgs> {};
+
+let
+    envname = "rtfd";
+    python = python27Full;
+    pyp = pkgs.python27Packages;
+in
+
+buildPythonPackage { 
+  name = "${envname}-env";
+  buildInputs = with pyp; [
+     python
+     stdenv
+     virtualenv
+     recommonmark
+     setuptools
+     numpydoc
+     readline
+  ];
+  src = ./.;
+  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+  # When used as `nix-shell --pure`
+  shellHook = ''
+  export NIX_ENV="[${envname}] "
+  exec zsh
+  '';
+  # used when building environments
+  extraCmds = ''
+  export NIX_ENV="[${envname}] "
+  exec zsh
+  '';
+  buildPhase = ''
+    make html
+  '';
+  installPhase = ''
+    cp -pr _build/html $out
+  '';
+}
+
