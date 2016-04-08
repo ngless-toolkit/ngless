7ffe3ab67606b5d496b44cf9783f2b53f67a1629
Luis Pedro Coelho <luis@luispedro.org>
2016-04-08 10:15:06 +0200

    BUG Add forgotten 'here' utility
    This makes it easier to have large strings in the code as heredocs

---
+++ NGLess/Utils/Here.hs
@@ -0,0 +1,13 @@
+module Utils.Here (here) where
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+
+here = QuasiQuoter
+    { quoteExp = stringE . skipStartNL
+    , quotePat = const (error "cannot be used as pattern")
+    , quoteType = const (error "cannot be used as type")
+    , quoteDec = const (error "cannot be used as declaration")
+    }
+
+skipStartNL ('\n':rest) = rest
+skipStartNL s = s
