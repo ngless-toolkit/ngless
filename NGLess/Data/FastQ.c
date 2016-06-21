143ea550cd1038165a6ad418d7368cc8ed9d18b1
Luis Pedro Coelho <luis@luispedro.org>
2016-06-21 16:33:07 +0200

    MIN Use more traditional C
    It seems travis has an older C compiler which does not like declaring
    variables in for loops.

--- NGLess/Data/FastQ.c
+++ NGLess/Data/FastQ.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 void updateCharCount(unsigned int n, char* pc, long int* v) {
+    unsigned int i;
     unsigned char* p = (unsigned char*)pc;
-    for (unsigned int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         ++v[p[i]];
     }
 }
 void updateQualityCounts(unsigned int n, char* qsc, long int* v) {
+    unsigned int i;
     unsigned char* qs = (unsigned char*)qsc;
-    for (unsigned int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         int qi = 256*i + qs[i];
         ++v[qi];
     }
