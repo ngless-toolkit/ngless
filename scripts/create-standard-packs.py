6bce144298c1c48d8e4481c797a3290102b8bce9
Luis Pedro Coelho <luis@luispedro.org>
2016-08-15 15:52:43 +0200

    ENH Update standard genomes. Add OM-RGC

--- scripts/create-standard-packs.py
+++ scripts/create-standard-packs.py
@@ -1,18 +1,17 @@
-ensembl_version="75"
-
 datasets = [
-    ('homo_sapiens', "Homo_sapiens.GRCh37", "hg19"),
-    ('saccharomyces_cerevisiae', "Saccharomyces_cerevisiae.R64-1-1", "sacCer3"),
-    ('caenorhabditis_elegans', "Caenorhabditis_elegans.WBcel235", "ce10"),
-    ('drosophila_melanogaster', "Drosophila_melanogaster.BDGP5", "dm3"),
-    ('gallus_gallus', "Gallus_gallus.Galgal4", "gg4"),
-    ('canis_familiaris', "Canis_familiaris.CanFam3.1", "cm3"),
-    ('bos_taurus', "Bos_taurus.UMD3.1", "bosTau4"),
-    ('rattus_norvegicus', "Rattus_norvegicus.Rnor_5.0", "rn4"),
-    ('mus_musculus', "Mus_musculus.GRCm38", "mm10"),
+    (85, 'homo_sapiens', "Homo_sapiens.GRCh38", "hg38"),
+    (75, 'homo_sapiens', "Homo_sapiens.GRCh37", "hg19"),
+    (75, 'saccharomyces_cerevisiae', "Saccharomyces_cerevisiae.R64-1-1", "sacCer3"),
+    (75, 'caenorhabditis_elegans', "Caenorhabditis_elegans.WBcel235", "ce10"),
+    (75, 'drosophila_melanogaster', "Drosophila_melanogaster.BDGP5", "dm3"),
+    (75, 'gallus_gallus', "Gallus_gallus.Galgal4", "gg4"),
+    (75, 'canis_familiaris', "Canis_familiaris.CanFam3.1", "cm3"),
+    (75, 'bos_taurus', "Bos_taurus.UMD3.1", "bosTau4"),
+    (75, 'rattus_norvegicus', "Rattus_norvegicus.Rnor_5.0", "rn4"),
+    (75, 'mus_musculus', "Mus_musculus.GRCm38", "mm10"),
 ]
 
-for (base_name,build, outputname) in datasets:
+for (ensembl_version, base_name, build, outputname) in datasets:
     level="toplevel"
     if base_name in ["homo_sapiens", "mus_musculus"]:
         level="primary_assembly"
