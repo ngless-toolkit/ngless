fcc0b571500b0ad672970c800a4fdc6ba8594d21
Renato Alves <alves.rjc@gmail.com>
2016-12-12 04:16:08 +0100

    TST Running tests against embedded and static builds is now possible
    The three options are now:
        run-tests.sh           (regular build)
        run-tests-embedded.sh  (build with embedded binaries)
        run-tests-static.sh    (static build with embedded binaries)

--- run-tests.sh
+++ run-tests.sh
@@ -2,33 +2,61 @@
 
 shopt -s nullglob
 
+function remove_ngless_bin {
+    # Ensure that the directory where ngless unpacks embedded binaries is removed (we want to test the embedded blobs)
+    NGLDIR="$HOME/.local/share/ngless/bin"
+    [ -d "$NGLDIR" ] && echo "Removing $NGLDIR to ensure embedded bins are used" && rm -rf "$NGLDIR"
+}
+
 # This script is located on the root of the repository
 # where samtools and bwa are also compiled
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export NGLESS_SAMTOOLS_BIN=$REPO/samtools-1.3.1/samtools
-export NGLESS_BWA_BIN=$REPO/bwa-0.7.15/bwa
+if [[ "$0" == *-embedded.sh ]]; then
+    echo ">>> Testing NGLess ( with embedded binaries ) <<<"
+    MAKETARGET="ngless-embed"
+    remove_ngless_bin
+elif [[ "$0" == *-static.sh ]]; then
+    echo ">>> Testing NGLess ( static build with embedded binaries ) <<<"
+    MAKETARGET="static"
+    remove_ngless_bin
+else
+    echo ">>> Testing NGLess ( regular build ) <<<"
+    export NGLESS_SAMTOOLS_BIN=$REPO/samtools-1.3.1/samtools
+    export NGLESS_BWA_BIN=$REPO/bwa-0.7.15/bwa
+    MAKETARGET=""
+fi
 
 ok="yes"
 
 make
 if test $? -ne "0"; then
-   echo "ERROR IN 'make'"
-   ok=no
+    echo "ERROR IN 'make'"
+    ok=no
 fi
+# haskell unit tests
 make check
 if test $? -ne "0"; then
-   echo "ERROR IN 'make check'"
-   ok=no
+    echo "ERROR IN 'make check'"
+    ok=no
 fi
 
-ngless_bin=$PWD/$(stack path --dist-dir)/build/ngless/ngless
+# Our test target (if not the default) is then also used for the subsequent tests
+if [ "$MAKETARGET" != "" ]; then
+    make $MAKETARGET
+    if test $? -ne "0"; then
+        echo "ERROR IN 'make $MAKETARGET'"
+        ok=no
+    fi
+fi
+
+ngless_bin=$(stack path --local-install-root)/bin/ngless
 if ! test -x $ngless_bin ; then
     echo "Could not determine path for ngless (guessed $ngless_bin)"
     exit 1
 fi
 
-basedir=$PWD
+basedir=$REPO
 for testdir in tests/*; do
     if test -d $testdir; then
         if test -f ${testdir}/TRAVIS_SKIP -a x$TRAVIS = xtrue; then
