dc91c657501c4ba178f1af3b510eb389496c46ec
Renato Alves <alves.rjc@gmail.com>
2017-02-16 16:18:35 +0100

    TST Fix samtools tests when to samtools is in PATH

--- tests/samfile-select-sort/check.sh
+++ tests/samfile-select-sort/check.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-if ! diff <(samtools view -h output.bam) texpected.sam ; then
+if [ "$(which samtools 2>/dev/null)" == "" ]; then
+    SAMTOOLS="$NGLESS_SAMTOOLS_BIN"
+else
+    SAMTOOLS="$(which samtools)"
+fi
+
+if ! diff <($SAMTOOLS view -h output.bam) texpected.sam ; then
     exit 1
 fi
