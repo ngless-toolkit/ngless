a840e72ac968bb113bbd72ad50e3dab6c512fc23
Renato Alves <alves.rjc@gmail.com>
2017-02-28 02:49:27 +0100

    RFCT+ENH Extract common wrapper functions and add --feature to ngless-count

--- scripts/ngless-count.py
+++ scripts/ngless-count.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 from subprocess import Popen
 import sys
 import tempfile
+from ngless_wrap import ngl_prepare_options, ngl_prepare_payload
 
 try:
     import argparse
@@ -20,80 +21,47 @@ def parse_args():
                         help="SAM/BAM/CRAM file to count reads on")
     parser.add_argument("-o", "--output", required=True,
                         help="Output file/path for results")
+    parser.add_argument("-f", "--features",
+                        help="Feature to count")
     parser.add_argument("-m", "--multiple",
                         choices=["dist1", "all1", "1overN", "unique_only"],
-                        help="Output file/path for results")
+                        help="How to handle multiple mappers")
     parser.add_argument("--debug", action="store_true",
                         help="Prints the payload before submitting to ngless")
 
     return parser.parse_args()
 
 
-def prepare_options(args):
+def prepare(args):
     # NOTE this needs to match the arguments in parse_args and the targets in payload
     # commas at the beginning of each option are used when that section has
-    # parameters from other functions.
+    # parameters from other functions. The keys are iterated in alphabetical order
+    # so only the first needs to have a comma.
     options = {
         "sam_opts": {
             "input": "'{input}'",
         },
         "count_opts": {
-            "multiple": ", multiple={{{multiple}}}",
+            "features": ", features=['seqname']",
+            "multiple": "multiple={{{multiple}}}",
         },
         "write_opts": {
             "output": ", ofile='{output}'",
         },
     }
 
-    parsed_dict = vars(args)
+    ngl_options = ngl_prepare_options(args, options)
 
-    result = dict()
-
-    for category in options:
-        for opt in options[category]:
-            if category not in result:
-                result[category] = []
-
-            if parsed_dict[opt] is not None:
-                result[category].append(options[category][opt])
-
-    # Format list of options to string by joining options with commas
-    for category in result.keys():
-        result[category] = ", ".join(result[category])
-
-    # Ensure all categories exist in the final dict even if empty
-    for category in options:
-        if category not in result:
-            result[category] = ''
-
-    if args.debug:
-        from pprint import pprint
-        print(">>> Options processed internally")
-        print(args)
-        pprint(result)
-        print(">>> End of internal options")
-
-    return result
-
-
-def prepare_payload(args):
     payload_tpl = """\
 ngless "0.0"
 write(count(samfile({sam_opts}){count_opts}){write_opts})\
-""".format(**prepare_options(args))
+""".format(**ngl_options)
 
-    payload = payload_tpl.format(**vars(args))
-
-    if args.debug:
-        print(">>> NGLess will be called with the following script:")
-        print(payload)
-        print(">>> End of script")
-
-    return payload
+    return ngl_prepare_payload(args, payload_tpl)
 
 
 def ngless(args):
-    payload = prepare_payload(args)
+    payload = prepare(args)
 
     with tempfile.NamedTemporaryFile() as script:
         script.write(payload.encode("utf8"))
