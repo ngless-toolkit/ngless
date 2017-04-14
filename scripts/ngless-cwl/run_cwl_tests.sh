1130e2f0c8426b081d7697e0b142259af2e526ff
Renato Alves <alves.rjc@gmail.com>
2017-04-14 20:40:23 +0200

    BLD Get Travis to run with tox

--- scripts/ngless-cwl/run_cwl_tests.sh
+++ scripts/ngless-cwl/run_cwl_tests.sh
@@ -2,6 +2,11 @@
 
 FAILURES=0
 
+# WORKAROUND: For these tests to work as in production we'd need an ngless binary with embedded bwa and samtools
+# Since compiling this in Travis is currently difficult, we point ngless to the binary location
+export NGLESS_SAMTOOLS_BIN="$(readlink -f ../../samtools-1.4/samtools)"
+export NGLESS_BWA_BIN="$(readlink -f ../../bwa-0.7.15/bwa)"
+
 for script in cwl/*.cwl; do
     # --leave-outputs - to leave outputs on the tmpdir created by cwl-runner (we don't need them)
     # --preserve-environment - to ensure ngless is found
