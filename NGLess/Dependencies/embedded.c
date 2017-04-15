7acc7c28054a8d831fd37b1e52425402898f768d
Renato Alves <alves.rjc@gmail.com>
2017-04-15 05:26:02 +0200

    MIN Use consistent names for samtools/bwa binaries

--- NGLess/Dependencies/embedded.c
+++ NGLess/Dependencies/embedded.c
@@ -1,8 +1,8 @@
 #ifndef NO_EMBED_SAMTOOLS_BWA
 #include "samtools_data.c"
 #include "bwa_data.c"
-const unsigned char* get_samtools_data () { return samtools_1_4_samtools_static; }
-unsigned int get_samtools_len () { return samtools_1_4_samtools_static_len; }
+const unsigned char* get_samtools_data () { return samtools_1_4_ngless_samtools_static; }
+unsigned int get_samtools_len () { return samtools_1_4_ngless_samtools_static_len; }
 
 const unsigned char* get_bwa_data () { return bwa_0_7_15_ngless_bwa_static; }
 unsigned int get_bwa_len () { return bwa_0_7_15_ngless_bwa_static_len; }
