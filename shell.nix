9fc2be48899d7b7ea1392b39c61b886909018bf8
Luis Pedro Coelho <luis@luispedro.org>
2017-05-19 10:19:22 +0200

    BLD Add lzma dependency
    Newer htslib requires this

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
 
   name = "nglessEnv";
 
-  buildInputs = with haskell.packages.ghc801; [
+  buildInputs = with haskell.packages.ghc802; [
     stack
     wget
     ghc
@@ -13,6 +13,7 @@ stdenv.mkDerivation {
   propagatedBuildInputs = [
     zlib
     bzip2
+    lzma
     samtools
     bwa
   ];
@@ -20,16 +21,21 @@ stdenv.mkDerivation {
   STACK_IN_NIX_EXTRA_ARGS
       = " --extra-lib-dirs=${zlib.out}/lib"
       + " --extra-lib-dirs=${bzip2.out}/lib"
+      + " --extra-lib-dirs=${lzma.out}/lib"
       + " --extra-include-dirs=${zlib.out}/include"
       + " --extra-include-dirs=${bzip2.out}/include"
+      + " --extra-include-dirs=${lzma.out}/include"
   ;
   extraCmds = ''
     export LD_LIBRARY_PATH+=:${zlib.out}/lib
     export LD_LIBRARY_PATH+=:${bzip2.out}/lib
+    export LD_LIBRARY_PATH+=:${lzma.out}/lib
   '';
   shellHook = ''
     export LD_LIBRARY_PATH+=:${zlib.out}/lib
     export LD_LIBRARY_PATH+=:${bzip2.out}/lib
+    export LD_LIBRARY_PATH+=:${lzma.out}/lib
+
     export NGLESS_SAMTOOLS_BIN=${samtools}/bin/samtools
     export NGLESS_BWA_BIN=${bwa}/bin/bwa
   '';
