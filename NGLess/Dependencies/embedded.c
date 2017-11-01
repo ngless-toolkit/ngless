e2320073e9cdc9a47ee045a4dea91ba952375c5b
Renato Alves <alves.rjc@gmail.com>
2017-11-01 23:42:56 +0100

    MNT Bump versions of embedded references to match release 0.5.0

--- NGLess/Dependencies/embedded.c
+++ NGLess/Dependencies/embedded.c
@@ -3,11 +3,11 @@
 #include "bwa_data.c"
 #include "megahit_data.c"
 
-const unsigned char* get_samtools_data () { return samtools_1_4_ngless_0_0_0_samtools_static; }
-unsigned int get_samtools_len () { return samtools_1_4_ngless_0_0_0_samtools_static_len; }
+const unsigned char* get_samtools_data () { return samtools_1_4_ngless_0_5_0_samtools_static; }
+unsigned int get_samtools_len () { return samtools_1_4_ngless_0_5_0_samtools_static_len; }
 
-const unsigned char* get_bwa_data () { return bwa_0_7_15_ngless_0_0_0_bwa_static; }
-unsigned int get_bwa_len () { return bwa_0_7_15_ngless_0_0_0_bwa_static_len; }
+const unsigned char* get_bwa_data () { return bwa_0_7_15_ngless_0_5_0_bwa_static; }
+unsigned int get_bwa_len () { return bwa_0_7_15_ngless_0_5_0_bwa_static_len; }
 
 const unsigned char* get_megahit_data() { return megahit_1_1_1_megahit_packaged_tar_gz; }
 unsigned int get_megahit_len() { return megahit_1_1_1_megahit_packaged_tar_gz_len; }
