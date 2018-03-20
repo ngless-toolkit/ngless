06a8cd3d54e48e93c5d0c7d0204c56c4d3171c50
Luis Pedro Coelho <luis@luispedro.org>
2018-03-20 14:51:16 +0100

    BEN+MIN Generate faster benchmarks

--- test_samples/merge/generate.py
+++ test_samples/merge/generate.py
@@ -1,10 +1,11 @@
 from random import random
 for sparse in [False, True]:
     n = 'sp_sample_{}' if sparse else 'sample_{}'
+    kn_features = (64 if sparse else 8)
     for s in range(128):
         with open(n.format(s), 'wt') as output:
             output.write('\tsample_{}\n'.format(s))
-            for f in range(1024*64):
+            for f in range(1024*kn_features):
                 if random() < .01:
                     output.write("Feature_{}\t{}\n".format(f, random()*1000))
                 elif not sparse:
