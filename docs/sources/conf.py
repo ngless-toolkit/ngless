864cf115ea95141bbaa8f473d5dac0a5df85d102
Luis Pedro Coelho <luis@luispedro.org>
2018-10-16 05:15:00 +0200

    MIN+DOC Update copyright year

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ngless'
-copyright = u'2013-2017, NGLess Authors'
+copyright = u'2013-2018, NGLess Authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
