cbbeaf3746964e5c486adf04d49777bca76c8228
Luis Pedro Coelho <luis@luispedro.org>
2019-03-01 04:03:36 +0100

    TST Make test conform to test suite expectations

--- tests/write_compression/check.sh
+++ tests/write_compression/check.sh
@@ -5,12 +5,12 @@ set -e
 SAMTOOLS="$(ngless --print-path samtools)"
 
 echo "Comparing SAM"
-diff -q <(grep -v '^@PG' output.sam) expected.sam
+diff -q <(grep -v '^@PG' output.sam) texpected.sam
 echo "Comparing SAM.gz"
-diff -q <(zcat output.sam.gz | grep -v '^@PG') expected.sam
+diff -q <(zcat output.sam.gz | grep -v '^@PG') texpected.sam
 echo "Comparing SAM.bz2"
-diff -q <(bzcat output.sam.bz2 | grep -v '^@PG') expected.sam
+diff -q <(bzcat output.sam.bz2 | grep -v '^@PG') texpected.sam
 echo "Comparing SAM.zstd"
-diff -q <(zstdcat output.sam.zstd | grep -v '^@PG') expected.sam
+diff -q <(zstdcat output.sam.zstd | grep -v '^@PG') texpected.sam
 echo "Comparing BAM"
-diff -q <($SAMTOOLS view -h output.bam | grep -v '^@PG') expected.sam
+diff -q <($SAMTOOLS view -h output.bam | grep -v '^@PG') texpected.sam
