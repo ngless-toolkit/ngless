aa529495b73c029217c85a938088ed34e6985c58
Luis Pedro Coelho <luis@luispedro.org>
2020-07-04 20:52:06 +0200

    BUG Recover reproducibility wrt previous versions
    It is important to keep the order for downstream processes. This comes
    at a minor cost in increased code awkwardness, but that's an acceptable
    trade-off.

--- NGLess/BuiltinModules/LoadDirectory.hs
+++ NGLess/BuiltinModules/LoadDirectory.hs
@@ -20,7 +20,6 @@ import System.FilePath.Glob
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad
 import Data.Maybe
-import Data.Either (lefts, rights)
 import Data.Default (def)
 import Data.List (sort, nub, isInfixOf, isSuffixOf)
 
@@ -48,7 +47,7 @@ buildSingle m1
     | "pair.2" `isInfixOf` m1 = T.unpack $ T.replace "pair.2" "single" (T.pack m1)
     | otherwise = "MARKER_FOR_FILE_WHICH_DOES_NOT_EXIST"
 
-matchUp :: [FilePath] -> NGLess ([FilePath], [(FilePath, FilePath)], [(FilePath,FilePath, FilePath)])
+matchUp :: [FilePath] -> NGLess ([FilePath], [Either (FilePath, FilePath) (FilePath,FilePath, FilePath)])
 matchUp fqfiles = do
     let match1 :: FilePath -> Maybe (FilePath, FilePath)
         match1 fp = listToMaybe . flip mapMaybe pairedEnds $ \(p1,p2) -> if
@@ -57,7 +56,7 @@ matchUp fqfiles = do
                     | otherwise -> Nothing
         -- match1 returns repeated entries if both pair.1 and pair.2 exist, `nub` removes duplicate records
         matched1 = nub $ mapMaybe match1 fqfiles
-    paired23 <- forM matched1 $ \(m1,m2) -> do
+    paired <- forM matched1 $ \(m1,m2) -> do
         let singles = buildSingle m1
         unless (m1 `elem` fqfiles) $ throwDataError ("Cannot find match for file: " ++ m2)
         unless (m2 `elem` fqfiles) $ throwDataError ("Cannot find match for file: " ++ m1)
@@ -65,34 +64,33 @@ matchUp fqfiles = do
             if singles `elem` fqfiles
                 then Right (m1, m2, singles)
                 else Left (m1, m2)
-    let used = concat $ flip map paired23 $ \case
+    let used = concat $ flip map paired $ \case
                 Left (a,b) -> [a,b]
                 Right (a,b,c) -> [a,b,c]
         singletons = filter (`notElem` used) fqfiles
-    return (singletons, lefts paired23, rights paired23)
+    return (singletons, paired)
 
-mocatSamplePaired :: [FilePath] -> T.Text -> Bool -> NGLessIO [NGLessObject]
-mocatSamplePaired fqfiles encoding doQC = do
+loadDirectoryFiles :: [FilePath] -> T.Text -> Bool -> NGLessIO [NGLessObject]
+loadDirectoryFiles fqfiles encoding doQC = do
     let passthru = [("__perform_qc", NGOBool doQC), ("encoding", NGOSymbol encoding)]
         encodeStr = NGOString . T.pack
-    (singletons, paired, paired3) <- runNGLess $ matchUp fqfiles
+    (singletons, paired) <- runNGLess $ matchUp fqfiles
 
     singletons' <- forM singletons $ \f -> do
         outputListLno' InfoOutput ["load_fastq_directory found single-end sample '", f, "'"]
         executeFastq (encodeStr f) passthru
-    paired' <- forM paired $ \(m1,m2) -> do
-        outputListLno' InfoOutput [
-                        "load_fastq_directory found paired-end sample '",
-                        m1, "' - '", m2]
-        executePaired (encodeStr m1) (("second", encodeStr m2):passthru)
-    paired3' <- forM paired3 $ \(m1, m2, singles) -> do
+    paired' <- forM paired $ \match -> do
+        let (m1, m2, singlesMsg, singlesArgs) = case match of
+                Left (a, b) -> (a, b, "", [])
+                Right (a, b, singles) -> (a, b,
+                            "' with singles file '" ++ singles ++ "'",
+                            [("singles", encodeStr singles)])
         outputListLno' InfoOutput [
                         "load_fastq_directory found paired-end sample '",
                         m1, "' - '", m2,
-                        "' with singles file '" ++ singles ++ "'"
-                        ]
-        executePaired (encodeStr m1) (("second", encodeStr m2):("singles", encodeStr singles):passthru)
-    return $ singletons' ++ paired' ++ paired3'
+                        singlesMsg]
+        executePaired (encodeStr m1) (("second", encodeStr m2):(singlesArgs++passthru))
+    return $ singletons' ++ paired'
 
 
 executeLoad :: NGLessObject -> KwArgsValues -> NGLessIO NGLessObject
@@ -105,7 +103,7 @@ executeLoad (NGOString samplename) kwargs = do
         throwDataError ("Attempting to load directory '"++basedir++"', but directory does not exist.")
     fqfiles <- fmap (sort . concat) $ forM exts $ \pat ->
         liftIO $ namesMatching (basedir </> ("*." ++ pat))
-    args <- mocatSamplePaired fqfiles encoding qcNeeded
+    args <- loadDirectoryFiles fqfiles encoding qcNeeded
     executeGroup (NGOList args) [("name", NGOString samplename)]
 executeLoad _ _ = throwShouldNotOccur "load_fastq_directory got the wrong arguments."
 
--- Tests-Src/Tests/LoadFQDirectory.hs
+++ Tests-Src/Tests/LoadFQDirectory.hs
@@ -20,39 +20,41 @@ case_error_p2 = isError (matchUp ["sample/sample.pair.2.fq.bz2"])
 
 case_known_cases = forM_ known_cases check1
 
-check1 expected@(s, p, p3) = do
-    let fqs = s ++ concat [[a,b] | (a,b) <- p] ++ concat [[a,b,c] | (a,b,c) <- p3]
+check1 expected@(s, p) = do
+    let extract (Left (a,b)) = [a,b]
+        extract (Right (a,b,c)) = [a,b,c]
+        fqs = s ++ concatMap extract p
     fromRight (matchUp fqs) @?= expected
 
 known_cases = 
     --  Simple with ".single"
-    [ ([], [], [("sample/sample.pair.1.fq", "sample/sample.pair.2.fq", "sample/sample.single.fq")])
+    [ ([], [Right ("sample/sample.pair.1.fq", "sample/sample.pair.2.fq", "sample/sample.single.fq")])
 
     -- basic uncompressed
-    , (["sample/uncompressed.fq"], [], [])
+    , (["sample/uncompressed.fq"], [])
 
     -- basic compressed I
-    , (["sample/sample.fq.gz"], [], [])
+    , (["sample/sample.fq.gz"], [])
 
     -- basic compressed II
-    , (["sample/sample.fq.bz2"], [], [])
+    , (["sample/sample.fq.bz2"], [])
 
     -- Paired using "_[12].fastq"
-    , ([], [("sample/sample_1.fastq.gz","sample/sample_2.fastq.gz")], [])
+    , ([], [Left ("sample/sample_1.fastq.gz","sample/sample_2.fastq.gz")])
 
     -- paired+singles compressed
-    , ([], [], [("sample/sample.pair.1.fq.gz", "sample/sample.pair.2.fq.gz", "sample/sample.single.fq.gz")])
+    , ([], [Right ("sample/sample.pair.1.fq.gz", "sample/sample.pair.2.fq.gz", "sample/sample.single.fq.gz")])
 
     -- paired+singles compressed II (bz2 + deeper directory)
-    , ([], [], [("mocat_sample_bz2_paired/sample/sample.pair.1.fq.bz2", "mocat_sample_bz2_paired/sample/sample.pair.2.fq.bz2", "mocat_sample_bz2_paired/sample/sample.single.fq.bz2")])
+    , ([], [Right ("mocat_sample_bz2_paired/sample/sample.pair.1.fq.bz2", "mocat_sample_bz2_paired/sample/sample.pair.2.fq.bz2", "mocat_sample_bz2_paired/sample/sample.single.fq.bz2")])
 
     -- _[FR] pairing+single
     -- Arguably, the pairing is not the expected one, but this was what was done before
-    , (["sample/sample.single.fq.gz"], [("sample/sample.pair_F.fq.gz", "sample/sample.pair_R.fq.gz")], [])
+    , (["sample/sample.single.fq.gz"], [Left ("sample/sample.pair_F.fq.gz", "sample/sample.pair_R.fq.gz")])
 
     -- mixed
     , (["sample/sampleC.single.fq.bz2"],
-            [("sample/sampleB.pair.1.fq.bz2", "sample/sampleB.pair.2.fq.bz2")],
-            [("sample/sampleA.pair.1.fq.bz2", "sample/sampleA.pair.2.fq.bz2", "sample/sampleA.single.fq.bz2")])
+            [Left  ("sample/sampleB.pair.1.fq.bz2", "sample/sampleB.pair.2.fq.bz2")
+            ,Right ("sample/sampleA.pair.1.fq.bz2", "sample/sampleA.pair.2.fq.bz2", "sample/sampleA.single.fq.bz2")])
     ]
 
