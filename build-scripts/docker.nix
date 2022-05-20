475785d4e26920e0375934ffc146dca60ee104e3
Luis Pedro Coelho <luis@luispedro.org>
2022-05-21 01:02:08 +0200

    BLD Use nix to build docker image

---
+++ build-scripts/docker.nix
@@ -0,0 +1,34 @@
+let
+ release = import ../release.nix;
+ pkgs = release.pkgs;
+
+in pkgs.dockerTools.buildImage {
+
+  name = "ngless-docker";
+
+  contents = [
+    release.NGLess.components.exes.ngless
+    pkgs.prodigal
+    pkgs.samtools
+    pkgs.megahit
+    pkgs.python3 # required for megahit (dependency should be upstreamed)
+    pkgs.minimap2
+    pkgs.bwa
+
+    pkgs.xz
+    pkgs.which
+    pkgs.zstd
+  ];
+
+  config = {
+      Env = [
+        "NGLESS_SAMTOOLS_BIN=/bin/samtools"
+        "NGLESS_BWA_BIN=/bin/bwa"
+        "NGLESS_PRODIGAL_BIN=/bin/prodigal"
+        "NGLESS_MEGAHIT_BIN=/bin/megahit"
+        "NGLESS_MINIMAP2_BIN=/bin/minimap2"
+      ];
+    };
+
+
+}
