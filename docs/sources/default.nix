f35d387352b543c339ff84057c1baa7192e1e8f7
Luis Pedro Coelho <luis@luispedro.org>
2022-06-04 12:10:28 +0200

    BLD Use same nixpkgs pin as release.nix
    Update the build script to the new API

--- docs/sources/default.nix
+++ docs/sources/default.nix
@@ -1,16 +1,15 @@
 #!/usr/bin/env nix-shell
 
 with (import (builtins.fetchTarball {
-  name = "nixml-stable-19.04";
-  url = https://github.com/nixos/nixpkgs/archive/37694c8cc0e9ecab60d06f1d9a2fd0073bcc5fa3.tar.gz;
-  sha256 = "1ibjg6ln2y764wfg0yd0v055jm4xzhmxy5lfz63m6jr3y16jdmzb";
+  name = "nixpksg-pinned";
+  url = "https://github.com/nixos/nixpkgs/archive/1bb17332abf7a782ed7fa3101fd7ba54dd239694.tar.gz";
+  sha256 = "17y5xb109vnkvqqf8nlx6b7as3f9p4v7z6xjfq9mg3vyyhym33fl";
 }) {});
-
 
 let
     envname = "rtfd";
-    python = python36Full;
-    pyp = pkgs.python36Packages;
+    python = python39Full;
+    pyp = pkgs.python39Packages;
     guzzle = python.pkgs.buildPythonPackage rec {
         pname = "guzzle_sphinx_theme";
         version = "0.1";
@@ -51,7 +50,7 @@ pyp.buildPythonPackage {
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
 
   buildPhase = ''
-    make html
+    make html SPHINXBUILD=${sphinx}/bin/sphinx-build
   '';
   installPhase = ''
     cp -pr _build/html $out
